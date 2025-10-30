17811cbef1b6dc0689918a2a118517ab04284154
Emanuele <eman.devito@studenti.unina.it>
2025-10-30 22:09:37 +0100

    Update armando_hardware.launch.py

--- launch/armando_hardware.launch.py
+++ launch/armando_hardware.launch.py
@@ -47,7 +47,7 @@ def generate_launch_description():
         output='screen',
         arguments=['-topic', 'robot_description', 
                    '-name', 'armando', 
-                   '-allow_renaming', 'true',],
+                   '-allow_renaming', 'true','-z','1'],
     )
     ign = [gazebo_ignition, gz_spawn_entity]
     # --- Controllers 
